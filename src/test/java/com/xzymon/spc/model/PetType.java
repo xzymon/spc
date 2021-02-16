aca0ad40fbd0c5f4963be8898d11094390efe49a
Szymon Ignaciuk <xzymon@gmail.com>
2021-02-16 20:17:13 +0100

    Adding POJOs, Closes #2.

---
+++ src/test/java/com/xzymon/spc/model/PetType.java
@@ -0,0 +1,14 @@
+package com.xzymon.spc.model;
+
+public class PetType {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/test/java/com/xzymon/spc/model/Vet.java
@@ -0,0 +1,5 @@
+package com.xzymon.spc.model;
+
+public class Vet extends Person {
+
+}
