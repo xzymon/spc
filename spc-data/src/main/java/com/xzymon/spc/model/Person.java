501a26609a31dc1c1c47c524d9cc73a525d1c877
xzymon <xzymon@gmail.com>
2021-02-17 19:57:28 +0100

    Adding BaseEntity. Closes #5

--- spc-data/src/main/java/com/xzymon/spc/model/Person.java
+++ spc-data/src/main/java/com/xzymon/spc/model/Person.java
@@ -1,6 +1,6 @@
 package com.xzymon.spc.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
 	private String firstName;
 	private String lastName;
--- spc-data/src/main/java/com/xzymon/spc/model/PetType.java
+++ spc-data/src/main/java/com/xzymon/spc/model/PetType.java
@@ -1,6 +1,6 @@
 package com.xzymon.spc.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
 	private String name;
 
