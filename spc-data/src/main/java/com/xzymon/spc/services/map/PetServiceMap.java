5408f3d06d4896e58d49dda6c74102e70a6d521d
xzymon <xzymon@gmail.com>
2021-03-10 18:18:07 +0100

    Impl Map based Services. Closes #6

---
+++ spc-data/src/main/java/com/xzymon/spc/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.xzymon.spc.services.map;
+
+import com.xzymon.spc.model.Pet;
+import com.xzymon.spc.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+	@Override
+	public Set<Pet> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}
